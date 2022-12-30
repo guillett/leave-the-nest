4640a4a1899231a6f076ccfa7a79542cd5a8fcf9
Thomas Guillet <thomas@palya.fr>
2022-12-30 08:30:37 +0100

    Include URL matching in Mastodon account discovery

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -2,8 +2,24 @@
 
 import { getAccount } from "../lib/mastodon.ts"
 
-const f = (field: string) =>
-  field ? field.match(/(?<n>@?[A-Za-zÀ-ÖØ-öø-ÿ_0-9]+@[\w\.]+)/g) || [] : []
+const url = (field: string) => {
+  const urls = field ? field.match(/htt?tps:\/\/[\w\.\/@_0-9]+/g) || [] : []
+  return urls
+    .map((u) => {
+      var groups = u.match(
+        /\/\/(?<host>[\w\.]+)\/@(?<username>[\w\.]+)/
+      )?.groups
+      if (groups) {
+        return `${groups.username}@${groups.host}`
+      }
+    })
+    .filter((id) => id)
+}
+
+const direct = (field: string) =>
+  field ? field.match(/[A-Za-zÀ-ÖØ-öø-ÿ_0-9]+@[\w\.]+/g) || [] : []
+
+const f = (field: string) => [].concat(...direct(field), ...url(field))
 
 export function addMastodonHandles(twitterData: any) {
   const found = []
@@ -15,21 +31,31 @@ export function addMastodonHandles(twitterData: any) {
       items.forEach((i) => {
         pre[type][i.id] = i
         i.mastodonIds = [].concat(
-          ...f(i.text) /*,
-					...(i?.entities?.urls?.map?.(u => [].concat(
-						f(u.title)
-						)))//*/
+          ...f(i.text),
+          ...(i?.entities?.urls?.map?.((u) => [].concat(f(u.title))) || [])
         )
         found.push(...i.mastodonIds)
       })
     })
   }
   twitterData.data.forEach((i) => {
+    var entityIds = []
+    if (i.entities) {
+      Object.values(i.entities).forEach((v) => {
+        v.urls?.forEach?.((u) => {
+          entityIds.push(...f(u.expanded_url))
+        })
+      })
+    }
+
     i.mastodonIds = []
       .concat(
         ...f(i.name),
-        ...f(i.description)
-        //...(i.pinned_tweet_id ? pre?.tweets[i.pinned_tweet_id]?.mastodonIds : []),
+        ...f(i.description),
+        ...(i.pinned_tweet_id
+          ? pre?.tweets?.[i.pinned_tweet_id]?.mastodonIds || []
+          : []),
+        ...[]
       )
       .map((id) => {
         const { acct } = getAccount(id)
